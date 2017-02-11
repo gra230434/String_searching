f13961dd9fa578786ceef2aafc615c401318b85e
Kevin W <gra230434@gmail.com>
2017-02-11 23:56:52 +0800

    add Exhaustive search

---
+++ Exhaustive.py
@@ -0,0 +1,26 @@
+def string_match(text, pattern):
+    text_len = len(text)
+    pattern_len = len(pattern)
+    for val in range(text_len):
+        match = True
+        if text_len - val >= pattern_len:
+            for paval in range(pattern_len):
+                if text[val+paval] != pattern[paval]:
+                    match = False
+                    break
+        else:
+            match = False
+            break
+        if match:
+            print(val)
+    pass
+
+
+def main():
+    text = 'ababcabc'
+    pattern = 'abc'
+    string_match(text, pattern)
+
+
+if __name__ == '__main__':
+    main()
